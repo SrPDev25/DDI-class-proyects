86901c33a7777c5fb1cb96885ebd2e38ec113538
SrPollo25 <ortegachkn25@gmail.com>
2022-11-11 13:22:50 +0100

    Arreglado el primer panel

--- Ejercicios/Trabajo3_Daniel_Ortega/WindowsFormsApp1/FormMain.Designer.cs
+++ Ejercicios/Trabajo3_Daniel_Ortega/WindowsFormsApp1/FormMain.Designer.cs
@@ -35,27 +35,28 @@
             this.btnGeneral = new System.Windows.Forms.Button();
             this.comboBoxCiudad = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBoxRealizarPedido = new System.Windows.Forms.GroupBox();
             this.btnVegetal = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBoxAddressDetails = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.comboBoxCalle = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.error = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel3 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.panel1.SuspendLayout();
             this.groupBoxRealizarPedido.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.error)).BeginInit();
             this.SuspendLayout();
             // 
             // imageList1
@@ -88,7 +89,7 @@
             // comboBoxCiudad
             // 
             this.comboBoxCiudad.FormattingEnabled = true;
-            this.comboBoxCiudad.Location = new System.Drawing.Point(177, 143);
+            this.comboBoxCiudad.Location = new System.Drawing.Point(177, 111);
             this.comboBoxCiudad.Name = "comboBoxCiudad";
             this.comboBoxCiudad.Size = new System.Drawing.Size(121, 21);
             this.comboBoxCiudad.TabIndex = 4;
@@ -104,12 +105,13 @@
             this.label1.TabIndex = 5;
             this.label1.Text = "Kebab Express";
             // 
-            // textBox1
+            // textBoxName
             // 
-            this.textBox1.Location = new System.Drawing.Point(177, 39);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 20);
-            this.textBox1.TabIndex = 6;
+            this.textBoxName.Location = new System.Drawing.Point(177, 39);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(121, 20);
+            this.textBoxName.TabIndex = 6;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
             // 
             // panel1
             // 
@@ -148,68 +150,49 @@
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.textBoxAddressDetails);
             this.panel2.Controls.Add(this.label9);
-            this.panel2.Controls.Add(this.textBox3);
-            this.panel2.Controls.Add(this.label8);
             this.panel2.Controls.Add(this.label7);
             this.panel2.Controls.Add(this.comboBoxCalle);
             this.panel2.Controls.Add(this.label6);
             this.panel2.Controls.Add(this.label5);
             this.panel2.Controls.Add(this.label4);
             this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBoxNumber);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.comboBoxCiudad);
             this.panel2.Controls.Add(this.groupBoxRealizarPedido);
-            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.textBoxName);
             this.panel2.Location = new System.Drawing.Point(0, 106);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(524, 236);
+            this.panel2.Size = new System.Drawing.Size(524, 207);
             this.panel2.TabIndex = 9;
             // 
-            // textBox4
+            // textBoxAddressDetails
             // 
-            this.textBox4.Location = new System.Drawing.Point(177, 197);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 20;
+            this.textBoxAddressDetails.Location = new System.Drawing.Point(177, 165);
+            this.textBoxAddressDetails.Name = "textBoxAddressDetails";
+            this.textBoxAddressDetails.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAddressDetails.TabIndex = 20;
+            this.textBoxAddressDetails.TextChanged += new System.EventHandler(this.textBoxAddressDetails_TextChanged);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label9.Location = new System.Drawing.Point(26, 196);
+            this.label9.Location = new System.Drawing.Point(26, 164);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(145, 18);
             this.label9.TabIndex = 19;
             this.label9.Text = "Puerta, piso, letra... *";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(177, 117);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 18;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label8.Location = new System.Drawing.Point(61, 116);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(104, 18);
-            this.label8.TabIndex = 17;
-            this.label8.Text = "Codigo postal ";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label7.Location = new System.Drawing.Point(120, 169);
+            this.label7.Location = new System.Drawing.Point(120, 137);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(51, 18);
             this.label7.TabIndex = 16;
@@ -218,17 +201,18 @@
             // comboBoxCalle
             // 
             this.comboBoxCalle.FormattingEnabled = true;
-            this.comboBoxCalle.Location = new System.Drawing.Point(177, 170);
+            this.comboBoxCalle.Location = new System.Drawing.Point(177, 138);
             this.comboBoxCalle.Name = "comboBoxCalle";
             this.comboBoxCalle.Size = new System.Drawing.Size(121, 21);
             this.comboBoxCalle.TabIndex = 15;
+            this.comboBoxCalle.SelectedIndexChanged += new System.EventHandler(this.comboBoxCalle_SelectedIndexChanged);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label6.Location = new System.Drawing.Point(107, 142);
+            this.label6.Location = new System.Drawing.Point(107, 110);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(64, 18);
             this.label6.TabIndex = 14;
@@ -267,12 +251,13 @@
             this.label3.TabIndex = 11;
             this.label3.Text = "Telefono *";
             // 
-            // textBox2
+            // textBoxNumber
             // 
-            this.textBox2.Location = new System.Drawing.Point(177, 65);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 10;
+            this.textBoxNumber.Location = new System.Drawing.Point(177, 65);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumber.TabIndex = 10;
+            this.textBoxNumber.TextChanged += new System.EventHandler(this.textBoxNumber_TextChanged);
             // 
             // label2
             // 
@@ -285,16 +270,28 @@
             this.label2.TabIndex = 9;
             this.label2.Text = "Nombre y apellidos *";
             // 
-            // Form1
+            // error
+            // 
+            this.error.ContainerControl = this;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(3, 106);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(518, 204);
+            this.panel3.TabIndex = 21;
+            // 
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.BlueViolet;
-            this.ClientSize = new System.Drawing.Size(523, 340);
+            this.ClientSize = new System.Drawing.Size(523, 315);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.Name = "FormMain";
             this.Text = "Kebab Express pedidos";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
@@ -303,6 +300,7 @@
             this.groupBoxRealizarPedido.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.error)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -314,7 +312,7 @@
         private System.Windows.Forms.Button btnGeneral;
         private System.Windows.Forms.ComboBox comboBoxCiudad;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxName;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.GroupBox groupBoxRealizarPedido;
         private System.Windows.Forms.Button btnVegetal;
@@ -323,14 +321,14 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.TextBox textBoxAddressDetails;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.ComboBox comboBoxCalle;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ErrorProvider error;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
--- Ejercicios/Trabajo3_Daniel_Ortega/WindowsFormsApp1/FormMain.cs
+++ Ejercicios/Trabajo3_Daniel_Ortega/WindowsFormsApp1/FormMain.cs
@@ -13,11 +13,12 @@ namespace WindowsFormsApp1
 {
     public partial class FormMain : Form
     {
+        Boolean[] isFine;
         String[] callesList;
         public FormMain()
         {
             InitializeComponent();
-            
+            isFine = new bool[] {false,false,false,false,false};
             
         }
 
@@ -32,16 +33,95 @@ namespace WindowsFormsApp1
             if (comboBoxCiudad.SelectedIndex!=-1)
             {
                 fillComboBox();
+                isFine[3] = true;
+                
             }
+            else
+            {
+                isFine[3] = false;
+                error.SetError(comboBoxCiudad, "Dato obligatorio");
+            }
+            CheckData();
+
+
+        }
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            error.SetError(textBoxName, "");
+            if (!IsAllNonDigits(textBoxName.Text))
+            {
+                isFine[0] = false;
+                error.SetError(textBoxName, "Nombre no válido");
+            }
+            else
+            {
+                isFine[0] = true;
+                
+            }
+            CheckData();
         }
 
-        private Boolean CheckData()
+        private void textBoxNumber_TextChanged(object sender, EventArgs e)
         {
-            Boolean isFine=true;
+            error.SetError(textBoxNumber, "");
+            if (textBoxNumber.Text.Length != 9 || !IsAllDigits(textBoxNumber.Text))
+            {
+                isFine[1] = false;
+                error.SetError(textBoxNumber, "Telefono no válido");
+            }
+            else
+            {
+                isFine[1] = true;
+                
+            }
+            CheckData();
+        }
 
+        private void comboBoxCalle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            error.SetError(comboBoxCalle, "");
+            if (comboBoxCalle.SelectedIndex == -1)
+            {
+                isFine[4] = false;
+                error.SetError(comboBoxCalle, "Dato obligatorio");
+            }
+            else
+            {
+                isFine[4] = true;
+                
+            }
+            CheckData();
+        }
 
+        private void textBoxAddressDetails_TextChanged(object sender, EventArgs e)
+        {
+            error.SetError(textBoxAddressDetails, "");
+            if (textBoxAddressDetails.Text.Length == 0)
+            {
+                isFine[2] = false;
+                error.SetError(textBoxAddressDetails, "Dato obligatório");
+            }
+            else
+            {
+                isFine[2] = true;
+                CheckData();
+            }
+            CheckData();
+        }
 
-            return isFine;
+        private void CheckData()
+        {
+            groupBoxRealizarPedido.Enabled = true;
+            foreach (Boolean fine in isFine)
+            {
+                if (!fine)
+                {
+                    groupBoxRealizarPedido.Enabled = false;
+                    break;
+                }
+            }
+            
+            
         }
 
         
@@ -55,6 +135,26 @@ namespace WindowsFormsApp1
             {
                 comboBoxCalle.Items.Add(calle);
             }
+        }
+
+        bool IsAllDigits(string s)
+        {
+            foreach (char c in s)
+            {
+                if (!char.IsDigit(c))
+                    return false;
+            }
+            return true;
+        }
+
+        bool IsAllNonDigits(string s)
+        {
+            foreach (char c in s)
+            {
+                if (char.IsDigit(c))
+                    return false;
+            }
+            return true;
         }
 
         
