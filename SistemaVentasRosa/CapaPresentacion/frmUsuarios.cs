5a33812f5858e71c627cf845c81a54d0674278d6
Daniel Ortega <ortegachkn25@gmail.com>
2023-01-14 17:24:08 +0100

    Empiezo trabajo rosa

---
+++ SistemaVentasRosa/CapaPresentacion/frmUsuarios.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CapaPresentacion.Utilidades;
+using CapaEntidad;
+using CapaNegocio;
+
+
+namespace CapaPresentacion
+{
+    public partial class frmUsuarios : Form
+    {
+        public frmUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void frmUsuarios_Load(object sender, EventArgs e)
+        {
+            cboestado.Items.Add(new OpcionCombo() { Valor = 1, Texto = "ACTIVO" });
+            cboestado.Items.Add(new OpcionCombo() { Valor = 0, Texto = "NO ACTIVO" });
+            cboestado.DisplayMember = "Texto";
+            cboestado.ValueMember = "Valor";
+            cboestado.SelectedIndex = 0;
+
+            List<Rol> listaRol = new CN_Rol().Listar();
+
+            foreach (Rol item in listaRol)
+            {
+                cborol.Items.Add(new OpcionCombo() { Valor = item.IdRol, Texto = item.Descripcion });
+            }
+            cborol.DisplayMember = "Texto";
+            cborol.ValueMember = "Valor";
+            cborol.SelectedIndex = 0;
+
+            foreach (DataGridViewColumn columna in dgvdata.Columns)
+            {
+                if (columna.Visible == true && columna.Name != "btnseleccionar")
+                    cbobusqueda.Items.Add(new OpcionCombo() { Valor = columna.Name, Texto = columna.HeaderText });
+            }
+            cbobusqueda.DisplayMember = "Texto";
+            cbobusqueda.ValueMember = "Valor";
+            cborol.SelectedIndex = 0;
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            dgvdata.Rows.Add(new object[]{"", txtId.Text, txtdocumento.Text, txtnombrecompleto.Text, txtcorreo.Text, txtclave.Text,
+                ((OpcionCombo)cborol.SelectedItem).Valor.ToString(),
+                 ((OpcionCombo)cborol.SelectedItem).Texto.ToString(),
+                  ((OpcionCombo)cboestado.SelectedItem).Valor.ToString(),
+                     ((OpcionCombo)cboestado.SelectedItem).Texto.ToString()
+            });
+
+            Limpiar();
+        }
+
+        private void Limpiar()
+        {
+            txtId.Text = "0";
+            txtdocumento.Text = "";
+            txtnombrecompleto.Text = "";
+            txtcorreo.Text = "";
+            txtclave.Text = "";
+            txtconfirmarclave.Text = "";
+            cborol.SelectedIndex = 0;
+            cboestado.SelectedIndex = 0;
+
+            //para que situe el foco en el primer combobox de usuario   
+            txtdocumento.Select();
+        }
+
+     
+    }
+}
