eca94e40863449f92fd31ac332f43f1071e79306
Daniel Ortega <ortegachkn25@gmail.com>
2023-01-15 23:57:44 +0100

    Termine el 5

---
+++ SistemaVentas_DanielOrtega/CapaDatos/CD_Usuario.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidad;
+using System.Reflection;
+using System.Windows.Controls.Primitives;
+
+
+namespace CapaDatos
+{
+    public class CD_Usuario
+    {
+        public List<Usuario> Listar() {
+            List<Usuario> lista = new List<Usuario>();
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena)) {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("SELECT u.IdUsuario, u.Documento, u.NombreCompleto, u.Correo, u.Clave, u.Estado, r.IdRol, r.Descripcion FROM usuario u ");
+                    query.AppendLine("inner join rol r ON u.idRol = r.IdRol");
+                    SqlCommand cmd = new SqlCommand(query.ToString(), oconexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader()) {
+                        while (dr.Read()) {//Va leyendo la siguiente row hasta que no hay ninguna y da false
+                            lista.Add(new Usuario()
+                            {
+                                IdUsuario = Convert.ToInt32(dr["IdUsuario"]),
+                                Documento = dr["Documento"].ToString(),
+                                NombreCompleto = dr["NombreCompleto"].ToString(),
+                                Correo = dr["Correo"].ToString(),
+                                Clave = dr["Clave"].ToString(),
+                                Estado = Convert.ToBoolean(dr["Estado"]),
+                                oRol=new Rol() { IdRol = Convert.ToInt32(dr["IdRol"]), Descripcion = dr["Descripcion"].ToString() }
+                            });
+
+                        }
+                    }
+                }
+                catch (Exception ex) {
+                    lista = new List<Usuario>();
+                }
+            }
+            return lista;
+        }
+    }
+}
---
+++ SistemaVentas_DanielOrtega/CapaPresentacion/frmUsuarios.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CapaPresentacion.Utilidades;
+using CapaEntidad;
+using CapaNegocio;
+
+
+namespace CapaPresentacion
+{
+    public partial class frmUsuarios : Form
+    {
+        public frmUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void frmUsuarios_Load(object sender, EventArgs e)
+        {
+            //Carga el comboBox estado
+                //Items
+            cboestado.Items.Add(new OpcionCombo() { Valor = 1, Texto = "ACTIVO" });
+            cboestado.Items.Add(new OpcionCombo() { Valor = 0, Texto = "NO ACTIVO" });
+                //Texto
+            cboestado.DisplayMember = "Texto";
+            cboestado.ValueMember = "Valor";
+                //Combo seleccionado por defecto
+            cboestado.SelectedIndex = 0;
+
+            List<Rol> listaRol = new CN_Rol().Listar();
+
+            foreach (Rol item in listaRol)
+            {
+                cborol.Items.Add(new OpcionCombo() { Valor = item.IdRol, Texto = item.Descripcion });
+            }
+            cborol.DisplayMember = "Texto";
+            cborol.ValueMember = "Valor";
+            cborol.SelectedIndex = 0;
+
+            foreach (DataGridViewColumn columna in dgvdata.Columns)
+            {
+                if (columna.Visible == true && columna.Name != "btnseleccionar")
+                    cbobusqueda.Items.Add(new OpcionCombo() { Valor = columna.Name, Texto = columna.HeaderText });
+            }
+            cbobusqueda.DisplayMember = "Texto";
+            cbobusqueda.ValueMember = "Valor";
+            cborol.SelectedIndex = 0;
+
+            List<Usuario> listaUsuario = new CN_Usuario().Listar();
+            foreach (Usuario item in listaUsuario)
+            {
+                dgvdata.Rows.Add(new object[] {"",item.IdUsuario,item.Documento,item.NombreCompleto,
+                    item.Correo,item.Clave,
+                    item.oRol.IdRol,
+                    item.oRol.Descripcion,
+                    item.Estado==true? 1:0,
+                    item.Estado==true ? "Activo":"No Activo"
+                });
+            }
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            dgvdata.Rows.Add(new object[]{"", txtId.Text, txtdocumento.Text, txtnombrecompleto.Text, txtcorreo.Text, txtclave.Text,
+                ((OpcionCombo)cborol.SelectedItem).Valor.ToString(),
+                 ((OpcionCombo)cborol.SelectedItem).Texto.ToString(),
+                  ((OpcionCombo)cboestado.SelectedItem).Valor.ToString(),
+                     ((OpcionCombo)cboestado.SelectedItem).Texto.ToString()
+            });
+
+            Limpiar();
+        }
+
+        private void Limpiar()
+        {
+            txtId.Text = "0";
+            txtdocumento.Text = "";
+            txtnombrecompleto.Text = "";
+            txtcorreo.Text = "";
+            txtclave.Text = "";
+            txtconfirmarclave.Text = "";
+            cborol.SelectedIndex = 0;
+            cboestado.SelectedIndex = 0;
+
+            //para que situe el foco en el primer combobox de usuario   
+            txtdocumento.Select();
+        }
+
+        private void dgvdata_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return; //Acaba el método?
+            if (e.ColumnIndex == 0)
+            {
+                e.Paint(e.CellBounds, DataGridViewPaintParts.All);
+
+                var w = 25;
+                var h = 25;
+                var x = e.CellBounds.Left + (e.CellBounds.Width - w) / 2;
+                var y = e.CellBounds.Top + (e.CellBounds.Width - h) / 2;
+
+                e.Graphics.DrawImage(Properties.Resources.check, new Rectangle(x, y, w, h));
+                e.Handled = true;
+            }
+        }
+
+        private void dgvdata_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvdata.Columns[e.ColumnIndex].Name == "btnseleccionar")
+            {
+                int indice = e.RowIndex;
+
+                if (indice >= 0)
+                {
+                    txtId.Text = dgvdata.Rows[indice].Cells["id"].Value.ToString();
+                    txtdocumento.Text = dgvdata.Rows[indice].Cells["Documento"].Value.ToString();
+                    txtnombrecompleto.Text = dgvdata.Rows[indice].Cells["NombreCompleto"].Value.ToString();
+                    txtcorreo.Text = dgvdata.Rows[indice].Cells["Correo"].Value.ToString();
+                    txtclave.Text = dgvdata.Rows[indice].Cells["Clave"].Value.ToString();
+                    txtconfirmarclave.Text = dgvdata.Rows[indice].Cells["Clave"].Value.ToString();
+
+                    foreach(OpcionCombo oc in cborol.Items)
+                    {
+                        if (Convert.ToInt32(oc.Valor) == Convert.ToInt32(dgvdata.Rows[indice].Cells["IdRol"].Value))
+                        {
+                            int indice_combo = cborol.Items.IndexOf(oc);
+                            cborol.SelectedIndex = indice_combo;
+                            break;
+                        }
+                    }
+
+                    foreach(OpcionCombo oc in cboestado.Items)
+                    {
+                        if (Convert.ToInt32(oc.Valor) == Convert.ToInt32(dgvdata.Rows[indice].Cells["EstadoValor"].Value))
+                        {
+                            int indice_combo = cboestado.Items.IndexOf(oc);
+                            cboestado.SelectedIndex = indice_combo;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
