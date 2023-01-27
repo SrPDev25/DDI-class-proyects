5c1014f0edacf2f3324e4e52a0a790a1d9ee43a2
Daniel Ortega <daniel.ortega.alu@santamaria-artesano.org>
2023-01-27 10:44:16 +0100

    en ello

--- SistemaVentas_DanielOrtega/CapaDatos/Properties/Settings.Designer.cs
+++ SistemaVentas_DanielOrtega/CapaDatos/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace CapaDatos.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,16 @@ namespace CapaDatos.Properties {
         public string BDSISTEMAVENTASConnectionString {
             get {
                 return ((string)(this["BDSISTEMAVENTASConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=205P15;Initial Catalog=BDSISTEMAVENTAS;User ID=sa")]
+        public string BDSISTEMAVENTASConnectionString1 {
+            get {
+                return ((string)(this["BDSISTEMAVENTASConnectionString1"]));
             }
         }
     }
